7b21d2de2f309e3aa9ee17bf10f7b9ecd396c629
Pierre Tardy <pierre.tardy@renault.com>
2024-07-23 15:47:15 +0200

    boilerplate

---
+++ src/option2comments.rs
@@ -0,0 +1,56 @@
+use std::{io::Write, path::PathBuf};
+
+use clap::Parser;
+use miette::IntoDiagnostic;
+use protox::Compiler;
+
+#[derive(Debug, clap::Parser)]
+pub struct Args {
+    /// The source file(s) to compile
+    #[clap(value_name = "PROTO_FILES", required = true, value_parser)]
+    files: Vec<PathBuf>,
+    /// The directory in which to search for imports.
+    #[clap(
+        short = 'I',
+        long = "include",
+        visible_alias = "proto_path_glob",
+        value_name = "PATH_GLOB",
+        default_value = ".",
+        value_parser
+    )]
+    includes: Vec<PathBuf>,
+    /// The output path to write the modified files to.
+    #[clap(
+        short = 'o',
+        long = "output",
+        visible_alias = "output_dir",
+        value_name = "PATH",
+        value_parser
+    )]
+    output: Option<PathBuf>,
+}
+fn main() -> miette::Result<()> {
+    miette::set_panic_hook();
+
+    let args = Args::parse();
+    let first_include = args.includes.get(0).expect("at least one include dir is expected").clone();
+    let mut compiler = Compiler::new(args.includes)?;
+    compiler.include_imports(false);
+    compiler.include_source_info(true);
+    let out_dir = args.output.or_else(|| Some(PathBuf::from("out"))).unwrap();
+    for file_glob in args.files {
+        let file_glob = file_glob.to_string_lossy();
+        let file_glob = shellexpand::tilde(&file_glob);
+        for file in glob::glob(&file_glob).into_diagnostic()? {
+            let file = file.into_diagnostic()?;
+            let relative = file.strip_prefix(&first_include).into_diagnostic()?;
+            let out_file = out_dir.join(relative);
+            std::fs::create_dir_all(out_file.parent().unwrap()).into_diagnostic()?;
+            let in_text = std::fs::read_to_string(&file).into_diagnostic()?;
+            let mut out = std::fs::File::create(out_file).into_diagnostic()?;
+            compiler.open_file(file)?;
+            out.write_all(in_text.as_bytes()).into_diagnostic()?;
+        }
+    }
+    Ok(())
+}
